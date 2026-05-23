89fe3868d0240b35f7f25ce113282bf5e67ce25e
Nicolas Bibeau <nicolasbibeau@tutamail.com>
2026-05-23 14:47:49 -0400

    Map files created

---
+++ lib/pages/map_page.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+import "package:site_see/widgets/camera_widget.dart";
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: CameraWidget(),
+    );
+  }
+}
